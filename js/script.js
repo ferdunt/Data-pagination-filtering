e86e95ce2be53f1da9e2681d93941c75876f989b
Fernando Guisbert Segales <guisbertfernando@gmail.com>
2020-09-14 17:53:25 -0400

    search implemented

--- js/script.js
+++ js/script.js
@@ -85,6 +85,100 @@ function addPagination(list) {
    });
 }
 
+/* 
+Create and append search component
+*/
+window.addEventListener('load', () => {
+   // 💬 Select the 'header' tag
+   const header = document.querySelector('.header');
+
+   // 💬 Create 'search' component and append to HTML
+   const searchComponent = `
+      <label for="search" class="student-search">
+         <input id="search" placeholder="Search by name...">
+         <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+      </label>
+   `;
+   header.insertAdjacentHTML("beforeend", searchComponent);
+
+   // 💬 Select and add event listener to search elements
+   const buttonSearchElement = document.querySelector('button[type="button"]');
+   const inputSearchElement = document.querySelector('#search');
+   buttonSearchElement.addEventListener('click', search);
+   inputSearchElement.addEventListener('keyup', search);
+
+});
+
+/* 
+Search the value typed in the search box
+*/
+function search() {
+   // 💬 Get the value in 'search' input in 'LowerCase'
+   const contentSearch = document.querySelector('#search').value.toLowerCase();
+
+   // 💬 Create a regex globally
+   const myRegex = new RegExp(contentSearch, 'g');
+
+   // 💬 Create a new array to store the new data
+   let studentsFiltered = [];
+
+   // 💬 Loop the 'data' for each person 
+   data.forEach(person => {
+      // 💬 Get the full name for each person
+      let fullName = `${person.name.first} ${person.name.last}`.toLowerCase();
+
+      // 💬 Search the name 
+      let dataFound = fullName.search(myRegex);
+
+      // 💬 Check if the name was found if it was we push into our new array
+      if (dataFound !== -1) {
+         studentsFiltered.push(person);
+      }
+   });
+
+   // 💬 Get the message 'Not Found'
+   const messageNoFound = document.querySelector('.js-message');
+   // 🚧 WIP 🚧
+   if (studentsFiltered.length === 0) {
+
+      // 💬 If the message of 'Not found' does not exist we create it
+      if (messageNoFound === null) {
+         const paginationSection = document.querySelector('.pagination');
+         const message = '<div class="js-message">No results found</div>';
+         paginationSection.insertAdjacentHTML("beforeend", message);
+      }
+
+      // 💬 Clear all the students and pagination
+      clear();
+   } else {
+      // 💬 Update the page and pagination with only the 'studentsFiltered'
+      showPage(studentsFiltered, 1);
+      addPagination(studentsFiltered);
+
+      // 💬 If the message of 'Not found' exists delete it
+      if (messageNoFound !== null) {
+         messageNoFound.remove();
+      }
+   }
+}
+
+/* 
+Clear the list of students and pagination
+*/
+function clear() {
+   // 💬 Select the 'student-list' ul tag to show the list
+   const studentList = document.querySelector('.student-list');
+
+   // 💬 Clean the 'studentList' 
+   studentList.innerHTML = '';
+
+   // 💬 Select the 'link-list' ul tag to show the pagination
+   const linkList = document.querySelector('.link-list');
+
+   // 💬 Clean the 'linkList'
+   linkList.innerHTML = '';
+}
+
 // Call functions
 showPage(data, 1);
 addPagination(data);
